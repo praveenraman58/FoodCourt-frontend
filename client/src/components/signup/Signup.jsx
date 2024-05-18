e5b9164bab347ba7ec3e265259afbd66a2a8f89c
Pravee <praveenraman58@gmail.com>
2024-05-18 16:16:43 +0530

    food court

--- client/src/components/signup/Signup.jsx
+++ client/src/components/signup/Signup.jsx
@@ -1,42 +1,40 @@
-import React from 'react'
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import classes from './signup.module.css'
-import img from '../../assets/womaneating.jpg'
-import { register } from '../../redux/authSlice'
-import { useDispatch } from 'react-redux'
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import classes from "./signup.module.css";
+import img from "../../assets/womaneating.jpg";
+import { register } from "../../redux/authSlice";
+import { useDispatch } from "react-redux";
 
 const Signup = () => {
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState(false)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleSignup = async(e) => {
-    e.preventDefault()
+  const handleSignup = async (e) => {
+    e.preventDefault();
     try {
       const res = await fetch(`http://localhost:5000/auth/register`, {
         headers: {
-          "Content-Type": 'application/json'
+          "Content-Type": "application/json",
         },
         method: "POST",
-        body: JSON.stringify({username, email, password})
-      })
+        body: JSON.stringify({ username, email, password }),
+      });
 
-      const data = await res.json()
-      dispatch(register(data))
-      navigate('/')
-      
+      const data = await res.json();
+      dispatch(register(data));
+      navigate("/");
     } catch (error) {
-      setError(true)
+      setError(true);
       setTimeout(() => {
-        setError(false)
-      }, 3000)
+        setError(false);
+      }, 3000);
     }
-  }
-
+  };
 
   return (
     <div className={classes.signUpContainer}>
@@ -47,23 +45,35 @@ const Signup = () => {
         <div className={classes.signUpRightSide}>
           <h2 className={classes.title}>Sign Up</h2>
           <form onSubmit={handleSignup} className={classes.signUpForm}>
-            <input type="text" placeholder="Type username" onChange={(e) => setUsername(e.target.value)}/>
-            <input type="email" placeholder="Type email" onChange={(e) => setEmail(e.target.value)}/>
-            <input type="password" placeholder="Type password"  onChange={(e) => setPassword(e.target.value)}/>
+            <input
+              type="text"
+              placeholder="Type username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="Type email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Type password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <button className={classes.submitBtn}>Sign Up</button>
-            <p>Already have an account? <Link to='/login'>Login</Link></p>
+            <p>
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
           </form>
-          {
-            error && (
-              <div className={classes.errorMessage}>
-                 Wrong credentials! Try different ones.
-              </div>
-            )
-          }
+          {error && (
+            <div className={classes.errorMessage}>
+              Wrong credentials! Try different ones.
+            </div>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
